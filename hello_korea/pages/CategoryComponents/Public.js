9174c49d9e6ca4fc32096ff7102f1df1a44d97e3
hansanga <cab08132@naver.com>
2023-08-13 19:36:09 +0900

    modify: 스타일 파일 위치 변경

--- hello_korea/pages/CategoryComponents/Public.js
+++ hello_korea/pages/CategoryComponents/Public.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import * as S from '../../styles/CategoryComponents/Public.styled';
 import Header from '../../components/MainComponents/Header';
+import Body from '../../components/CategoryComponents/Public/Body';
 
 export default function Public(){
    return (
       <S.Wrapper>
-         <Header />
          <S.Container>
-            <S.Title>Public Service</S.Title>
-            {/* 다른 컨텐츠 */}
+            <Header />
+            <Body />
          </S.Container>
       </S.Wrapper>
    );
--- hello_korea/pages/CategoryComponents/Traffic.js
+++ hello_korea/pages/CategoryComponents/Traffic.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import * as S from '../../styles/CategoryComponents/Traffic.styled';
 import Header from '../../components/MainComponents/Header';
+import Body from '../../components/CategoryComponents/Traffic/Body';
 
 
 export default function Traffic(){
    return (
       <S.Wrapper>
-         <Header />
+         <S.Container>
+            <Header />
+            <Body />
+         </S.Container>
       </S.Wrapper>
    );
 };
-
