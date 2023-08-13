b0d38cded0fb24a414838f71f233b1b01479a9a8
hansanga <cab08132@naver.com>
2023-08-13 19:37:50 +0900

    feat: Traffic Body 파일 추가

---
+++ hello_korea/components/CategoryComponents/Traffic/Body.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as S from "./Body.styled";
+import TipBox from '../../../components/TipComponents/TipBox';
+import Link from 'next/link'
+
+export default function Body(){
+  return (
+
+     <S.Container>
+        <S.Title>Traffic Service</S.Title>
+        <S.MenuBar>
+           <li>Map</li>
+           <li>Public</li>
+           <li>Navigation</li>
+           <li>Taxi</li>
+        </S.MenuBar>
+        <S.Line />
+        <TipBox />
+        <S.MenuBottom>
+            <S.MenuBottom1Icon src='\img\Subway.svg' />
+           <S.MenuBottom1Text1>KaKao Subway</S.MenuBottom1Text1>
+           <S.MenuBottom1Text2>The most popular Korean subway application</S.MenuBottom1Text2>
+        </S.MenuBottom>
+        <S.MenuBottom>
+            <S.MenuBottom1Icon src='\img\Traffic.svg' />
+            <S.MenuBottom1Text1>KaKao Bus</S.MenuBottom1Text1>
+        </S.MenuBottom>
+
+         <S.CopyrightContainer>
+            <S.Copyright>@ Cpoyright</S.Copyright>
+         </S.CopyrightContainer>
+
+        
+
+      </S.Container>
+      
+   
+  );
+};
