b5f8e55c277b98ab7ee1906c9e283ce58b674990
hansanga <cab08132@naver.com>
2023-08-13 19:37:28 +0900

    feat: Public Body파일추가

---
+++ hello_korea/components/CategoryComponents/Public/Body.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as S from "./Body.styled";
+import TipBox from '../../../components/TipComponents/TipBox';
+import Link from 'next/link'
+
+export default function Body(){
+  return (
+   
+<S.Container>
+        <S.Title>Public Service</S.Title>
+        <S.MenuBar>
+           <li>Comprehensive Certification</li>
+           <li>Safety</li>
+        </S.MenuBar>
+        <S.Line />
+        <TipBox />
+        <S.MenuBottom>
+           <S.MenuBottom1Icon src='\img\Subway.svg' />
+           <S.MenuBottom1Text1>KaKao Subway</S.MenuBottom1Text1>
+           <S.MenuBottom1Text2>The most popular Korean subway application</S.MenuBottom1Text2>
+        </S.MenuBottom>
+        <S.MenuBottom>
+            <S.MenuBottom1Icon src='\img\Traffic.svg' />
+            <S.MenuBottom1Text1>KaKao Bus</S.MenuBottom1Text1>
+        </S.MenuBottom>
+
+         <S.CopyrightContainer>
+            <S.Copyright>@ Cpoyright</S.Copyright>
+         </S.CopyrightContainer>
+
+        
+
+      </S.Container>
+      
+   
+  );
+};
