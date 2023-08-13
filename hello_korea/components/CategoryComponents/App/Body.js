06f7201f53dfb2fa0368fc831c81f6a1e2104b6a
hansanga <cab08132@naver.com>
2023-08-13 19:36:49 +0900

    feat: App Body파일 추가

---
+++ hello_korea/components/CategoryComponents/App/Body.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as S from "./Body.styled";
+import Link from 'next/link'
+
+export default function Body(){
+  return (
+   
+      <S.Container>
+
+         <S.CopyrightContainer>
+            <S.Copyright>@ Cpoyright</S.Copyright>
+         </S.CopyrightContainer>
+
+        
+
+      </S.Container>
+      
+   
+  );
+};
