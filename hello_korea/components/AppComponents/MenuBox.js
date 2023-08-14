8a300b37e171837d99a9c5207bf769e5cbf09aaa
hansanga <cab08132@naver.com>
2023-08-14 23:58:46 +0900

    feat: AppBox link 기능 추가

--- hello_korea/components/AppComponents/MenuBox.js
+++ hello_korea/components/AppComponents/MenuBox.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import * as S from "./MenuBox.styled"; 
+import Link from 'next/link'
 
-const MenuBox = ({ iconSrc, text1, text2 }) => (
+const MenuBox = ({ link, iconSrc, text1, text2 }) => (
+  <Link href={link}>
   <S.MenuBottom>
     <S.MenuBoxIcon src={iconSrc} />
     <S.MenuBox>
       <S.MenuBottom1Text1>{text1}</S.MenuBottom1Text1>
       <S.MenuBottom1Text2>{text2}</S.MenuBottom1Text2>
     </S.MenuBox>
-  </S.MenuBottom>
+  </S.MenuBottom></Link>
 );
 
 export default MenuBox;
