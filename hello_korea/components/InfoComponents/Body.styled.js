cee707847191746d97a4c1218e4432695e225009
hansanga <cab08132@naver.com>
2023-08-15 00:01:13 +0900

    feat: 앱 상세페이지 컴포넌트 분리
    - 앱 상세페이지 컴포넌트를 분리하여 각 페이지마다 재사용 가능하게 했음.

---
+++ hello_korea/components/InfoComponents/Body.styled.js
@@ -0,0 +1,108 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 73px;
+`;
+
+
+export const Title = styled.div`
+  display: flex;
+  padding: 0 30px;
+`;
+
+export const Icon = styled.img`
+  height: 22px;
+  margin-right:10px;
+`;
+
+export const AppName = styled.div`
+  font-family: 'Inter';
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  color: #686868;
+  display: flex;
+`;
+
+
+export const Line = styled.div`
+  position: absolute;
+  width: 274px;
+  height: 0px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #949494;
+  margin-top: 25px;
+`;
+
+export const Info = styled.div`
+  margin: 10px 0;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 9px;
+  line-height: 11px;
+  color: #737373;
+  padding: 0 30px;
+`;
+
+export const BtnZone = styled.div`
+  flex-direction: row;
+  padding: 10px 20px;
+  justify-content: space-between;
+  display: flex;
+  position: fixed;
+  bottom: 30px;
+`;
+
+export const Download = styled.button`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 11px;
+  line-height: 13px;
+  color: #737373;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.04);
+  border-radius: 10px;
+  border: none;
+  width: 139px;
+  height: 31px;
+  margin: 5px;
+  cursor: pointer;
+
+  & > img {
+    width: 10px;
+    height: 12px;
+  }
+`;
+
+export const Share = styled.button`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 11px;
+  line-height: 13px;
+  color: #737373;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.04);
+  border-radius: 10px;
+  border: none;
+  width: 139px;
+  height: 31px;
+  margin: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  & > img {
+    width: 15px;
+    height: 15px;
+    margin: 0 33px 0 10px;
+  }
+`;
