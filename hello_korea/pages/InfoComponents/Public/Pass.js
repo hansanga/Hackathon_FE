bab457f90a8045a7ce3344b5f2bd1e1f33400fda
hansanga <cab08132@naver.com>
2023-08-15 06:51:02 +0900

    feat: 앱 상세페이지 추가

--- hello_korea/pages/InfoComponents/Public/Pass.js
+++ hello_korea/pages/InfoComponents/Public/Pass.js
@@ -3,14 +3,14 @@ import * as S from '../../../styles/CategoryComponents/App.styled'
 import Header from '../../../components/InfoComponents/Header';
 import Body from '../../../components/InfoComponents/Body';
 
-export default function KaKaoBus(){
+export default function Pass(){
    return (
       <S.Wrapper>
          <S.Container>
             <Header />
             <Body
-               iconSrc1='\AppIcon\KaKaoBus.png'
-               appName='KaKao Bus'
+               iconSrc1='\AppIcon\Pass.png'
+               appName='Pass'
                text1='Kakao Bus is an app that provides real-time operation information, routes, traffic information, and stop location of buses, and also provides information on vacancies in the car only for direct/general seat buses in Gyeonggi-do.'
             />
          </S.Container>
--- hello_korea/pages/InfoComponents/Traffic/KakaoNavi.js
+++ hello_korea/pages/InfoComponents/Traffic/KakaoNavi.js
@@ -3,14 +3,14 @@ import * as S from '../../../styles/CategoryComponents/App.styled'
 import Header from '../../../components/InfoComponents/Header';
 import Body from '../../../components/InfoComponents/Body';
 
-export default function KaKaoSubway(){
+export default function KaKaoNavigation(){
    return (
       <S.Wrapper>
          <S.Container>
             <Header />
             <Body
-               iconSrc1='\AppIcon\KaKaoSubway.png'
-               appName='KaKao Subway'
+               iconSrc1='\AppIcon\KakaoNavi.png'
+               appName='KaKao Navigation'
                text1='Kakao Subway is an app that provides information on exits and neighborhoods of all subway stations across the country, and has the alarm function to notify you one stop before your destination, and the function to share the expected arrival time with your Kakao Talk friends.'
             />
          </S.Container>
