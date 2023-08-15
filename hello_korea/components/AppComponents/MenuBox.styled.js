60604f1fc0952d2bc9fe0b551d4e0ebd37e4cefe
hansanga <cab08132@naver.com>
2023-08-16 06:45:10 +0900

    feat: 해시태그 추가

--- hello_korea/components/AppComponents/MenuBox.styled.js
+++ hello_korea/components/AppComponents/MenuBox.styled.js
@@ -10,6 +10,7 @@ export const MenuBottom = styled.div`
   cursor: pointer;
   display: flex;
   align-items: center;
+  overflow-y: scoll;
 `;
 
 export const MenuBoxIcon = styled.img`
@@ -17,6 +18,7 @@ export const MenuBoxIcon = styled.img`
    height: 38px;
    position: absolute;
    margin: 0 10px;
+   border-radius: 5px;
 `;
 
 export const MenuBox = styled.div`
@@ -26,6 +28,7 @@ export const MenuBox = styled.div`
    display: flex;
 `;
 
+// 앱 이름
 export const MenuBottom1Text1 = styled.div`
   position: absolute;
   width: 137px;
@@ -40,6 +43,7 @@ export const MenuBottom1Text1 = styled.div`
   margin: 10px 60px;
 `;
 
+// 앱 간단설명
 export const MenuBottom1Text2 = styled.div`
   position: absolute;
   height: 8px;
@@ -50,5 +54,20 @@ export const MenuBottom1Text2 = styled.div`
   color: #737373;
   text-align: left;
   margin-top: 28px;
+  margin-left: 60px;
+`;
+
+// 해시태그
+export const MenuBottom1Text3 = styled.div`
+  position: absolute;
+  height: 8px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 8px;
+  line-height: 10px;
+  color: #A3A1A1;
+  font-family: Inter;
+  text-align: left;
+  margin-top: 45px;
   margin-left: 60px;
 `;
