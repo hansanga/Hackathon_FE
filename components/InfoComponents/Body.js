916d8dedabf0a963bc1d9ce8753aad3ddc44799b
DEGULL <skytmdgus99@gmail.com>
2023-08-16 17:12:37 +0900

    goverment download/share test

--- components/InfoComponents/Body.js
+++ components/InfoComponents/Body.js
@@ -2,34 +2,7 @@ import React from 'react';
 import * as S from './Body.styled';
 import Link from 'next/link';
 
-
-export default function Body({ iconSrc1, appName, text1 }) {
-    const handleShare = async () => {
-        try {
-            if (navigator.share) {
-                await navigator.share({
-                    title: 'catchKorea',
-                    text: '내용뭐라고,,,',
-                    url: window.location.href,
-                });
-            } else {
-                throw new Error('Web Share API not supported');
-            }
-        } catch (error) {
-            console.error('Error sharing:', error);
-            alert('Sharing failed. Please try again later.');
-        }
-    };
-
-    const handleDownload = () => {
-        // 갤럭시와 아이폰을 구분하여 각각의 앱 스토어로 이동
-        if (navigator.userAgent.match(/Android/i)) {
-            window.location.href = '링크_Google_Play_Store';
-        } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-            window.location.href = '링크_App_Store';
-        }
-    };
-
+export default function Body({ iconSrc1, appName, text1, handleDownload, handleShare }) {
     return (
         <S.Container>
             <S.Title>
--- pages/InfoComponents/Public/Comprehensive/Goverment24.js
+++ pages/InfoComponents/Public/Comprehensive/Goverment24.js
@@ -5,21 +5,28 @@ import Body from '../../../../components/InfoComponents/Body';
 
 export default function Goverment24() {
     const handleDownload = () => {
-        let downloadLink = '';
-        
         // 갤럭시와 아이폰을 구분하여 다운로드 링크 설정
         if (navigator.userAgent.match(/Android/i)) {
-            // 갤럭시의 Google Play Store 링크
-            downloadLink = '링크_Google_Play_Store';
+            window.location.href = '링크_Google_Play_Store';
         } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-            // 아이폰의 App Store 링크
-            downloadLink = '링크_App_Store';
+            window.location.href = '링크_App_Store';
         }
-        
-        if (downloadLink) {
-            window.location.href = downloadLink;
-        } else {
-            alert('Download link not available for your device.');
+    };
+
+    const handleShare = async () => {
+        try {
+            if (navigator.share) {
+                await navigator.share({
+                    title: 'catchKorea',
+                    text: '내용뭐라고,,,',
+                    url: window.location.href,
+                });
+            } else {
+                throw new Error('Web Share API not supported');
+            }
+        } catch (error) {
+            console.error('Error sharing:', error);
+            alert('Sharing failed. Please try again later.');
         }
     };
 
@@ -27,22 +34,17 @@ export default function Goverment24() {
         <S.Wrapper>
             <S.Container>
                 <Header />
-                <S.ImageContainer>
-                    <Body
-                        iconSrc1="\AppIcon\Goverment24.png"
-                        appName="Goverment 24"
-                        img="/img/Rectangle 12.png"
-                        text1={`
-                        Government 24 is an Internet government service integrated portal site operated by the South Korean government.
-                        There is a foreign service page, and services for foreign workers, married immigrants, and international students can be used through government 24.
+                <Body
+                    iconSrc1="\AppIcon\Goverment24.png"
+                    appName="Goverment 24"
+                    text1={`
+                    Government 24 is an Internet government service integrated portal site operated by the South Korean government.
+                    There is a foreign service page, and services for foreign workers, married immigrants, and international students can be used through government 24.
 
-                        How to download: You can install it from the app store or Play Store.`}
-                    />
-                    <S.Download onClick={handleDownload}>
-                        <S.Icon src="/img/Download.svg" />
-                        Link / Download
-                    </S.Download>
-                </S.ImageContainer>
+                    How to download: You can install it from the app store or Play Store.`}
+                    handleDownload={handleDownload}
+                    handleShare={handleShare}
+                />
             </S.Container>
         </S.Wrapper>
     );
