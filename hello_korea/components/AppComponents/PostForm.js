5d2bd301dc959b65421fefb642e9d243486796eb
hansanga <cab08132@naver.com>
2023-08-17 02:37:14 +0900

    modify: 주소변경

--- hello_korea/components/AppComponents/PostForm.js
+++ hello_korea/components/AppComponents/PostForm.js
@@ -1,25 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from "./PostForm.styled";
 
 
 function PostForm() {
   const [title, setTitle] = useState('');
   const [contents, setContents] = useState('');
-  const [serverLink, setServerLink] = useState('');
+  const [serviceLink, setServiceLink] = useState('');
   const [hashtag, setHashtag] = useState('');
+  // const [categories, setCategories] = useState([]);
+  // const [selectedCategory, setSelectedCategory] = useState('');
+
+  // useEffect(() => {
+  //   // 백엔드 API에서 카테고리 데이터 가져오기
+  //   fetch('/api/categories')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setCategories(data);
+  //       setSelectedCategory(data[0]); // 초기값 설정
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });
+  // }, []);
 
   const handleSubmit = () => {
+    // e.preventDefault();
     const hashtagArray = hashtag.split(',').map(tag => tag.trim());
     const requestData = {
       title,
       contents,
-      serverLink,
+      serviceLink,
       hashtag: hashtagArray
     };
 
+    // const postData = {
+    //   category: selectedCategory,
+    //   // 다른 게시물 정보 추가
+    // };
+
     const category_id = 123; 
 
-    fetch(`https://catchkorea-a5799a624288.herokuapp.com/post/{category_id}`, {
+    fetch(`https://catchkorea-a5799a624288.herokuapp.com/upload/post`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -37,6 +58,19 @@ function PostForm() {
 
   return (
     <S.FormWrapper onSubmit={handleSubmit}>
+      {/* <S.InputGroup>
+      <S.Label>카테고리</S.Label>
+        <select
+          value={selectedCategory}
+          onChange={e => setSelectedCategory(e.target.value)}
+        >
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </S.InputGroup> */}
       <S.InputGroup>
         <S.Label>제목</S.Label>
         <S.Input type="text" value={title} onChange={e => setTitle(e.target.value)} />
@@ -46,8 +80,8 @@ function PostForm() {
         <S.TextArea value={contents} onChange={e => setContents(e.target.value)} />
       </S.InputGroup>
       <S.InputGroup>
-        <S.Label>서버 링크</S.Label>
-        <S.Input type="text" value={serverLink} onChange={e => setServerLink(e.target.value)} />
+        <S.Label>서비스 링크</S.Label>
+        <S.Input type="text" value={serviceLink} onChange={e => setServiceLink(e.target.value)} />
       </S.InputGroup>
       <S.InputGroup>
         <S.Label>해시태그</S.Label>
