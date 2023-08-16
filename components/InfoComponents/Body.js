8cc150f3286e29823803899a7ab5bd69701914f3
DEGULL <skytmdgus99@gmail.com>
2023-08-16 15:56:35 +0900

    download/share test

--- components/InfoComponents/Body.js
+++ components/InfoComponents/Body.js
@@ -1,26 +1,34 @@
-// 앱 설명페이지 컴포넌트 분리
 import React from 'react';
-import * as S from "./Body.styled";
+import * as S from './Body.styled';
 import Link from 'next/link';
+
 
 export default function Body({ iconSrc1, appName, text1 }) {
     const handleShare = async () => {
-        if (navigator.share) {
-            try {
+        try {
+            if (navigator.share) {
                 await navigator.share({
                     title: 'My App',
                     text: 'Check out this amazing app!',
-                    url: window.location.href
+                    url: window.location.href,
                 });
-            } catch (error) {
-                console.error('Error sharing:', error);
+            } else {
+                throw new Error('Web Share API not supported');
             }
-        } else {
-            console.log('Web Share API not supported');
+        } catch (error) {
+            console.error('Error sharing:', error);
+            alert('Sharing failed. Please try again later.');
         }
     };
 
-    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+    const handleDownload = () => {
+        // 갤럭시와 아이폰을 구분하여 각각의 앱 스토어로 이동
+        if (navigator.userAgent.match(/Android/i)) {
+            window.location.href = '링크_Google_Play_Store';
+        } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+            window.location.href = '링크_App_Store';
+        }
+    };
 
     return (
         <S.Container>
@@ -29,13 +37,17 @@ export default function Body({ iconSrc1, appName, text1 }) {
                 <S.AppName>{appName}</S.AppName>
                 <S.Line />
             </S.Title>
-            <S.Info>
-                {text1}
-            </S.Info>
+            <S.Info>{text1}</S.Info>
             <S.BtnZone>
-                <S.Download><S.Icon src='/img/Download.svg' />Link / Download</S.Download>
-                {isMobile && <S.Share onClick={handleShare}><S.Icon src='/img/Share.svg' />Share</S.Share>}
+                <S.Download onClick={handleDownload}>
+                    <S.Icon src="/img/Download.svg" />
+                    Link / Download
+                </S.Download>
+                <S.Share onClick={handleShare}>
+                    <S.Icon src="/img/Share.svg" />
+                    Share
+                </S.Share>
             </S.BtnZone>
         </S.Container>
     );
-};
+}
