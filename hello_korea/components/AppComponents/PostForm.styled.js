8dac01053f4518aba206dfa6decd559b1c6122fd
hansanga <cab08132@naver.com>
2023-08-17 00:17:31 +0900

    feat: 게시물 작성 페이지

---
+++ hello_korea/components/AppComponents/PostForm.styled.js
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+
+export const FormWrapper = styled.form`
+  max-width: 500px;
+  margin-top: 100px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f7f7f7;
+`;
+
+export const InputGroup = styled.div`
+  margin-bottom: 15px;
+`;
+
+export const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+export const SubmitButton = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
