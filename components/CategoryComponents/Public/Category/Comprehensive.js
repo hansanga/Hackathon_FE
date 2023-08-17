adad37489f43f2327609731f6b4d3f6fdd72dd21
DEGULL <skytmdgus99@gmail.com>
2023-08-17 09:29:38 +0900

    수정1

--- components/CategoryComponents/Public/Category/Comprehensive.js
+++ components/CategoryComponents/Public/Category/Comprehensive.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MenuBox from '../../../../components/AppComponents/MenuBox';
-
+/*
 export default function Content() {
     const [menuData, setMenuData] = useState([]); // 메뉴 데이터를 저장할 상태
 
@@ -60,10 +60,7 @@ export default function Content() {
     );
 };
  
-/* 
-import React, { useState, useEffect } from 'react';
-import MenuBox from '../../../../components/AppComponents/MenuBox';
-import axios from 'axios';
+ */
 
 export default function Content() {
     const [menuData, setMenuData] = useState([]); // 메뉴 데이터를 저장할 상태
@@ -72,7 +69,7 @@ export default function Content() {
         // API에서 메뉴 데이터를 가져오는 함수
         const fetchMenuData = async () => {
             try {
-                const response = await axios.get('https://catchkorea-a5799a624288.herokuapp.com/post/{category_id}'); // API 엔드포인트
+                const response = await axios.get('https://catchkorea-a5799a624288.herokuapp.com/post/{category_id}'); 
                 setMenuData(response.data); // API에서 받아온 데이터를 상태에 저장
             } catch (error) {
                 console.error('Error fetching menu data:', error);
@@ -86,15 +83,14 @@ export default function Content() {
         <>
             {menuData.map(menu => (
                 <MenuBox
-                    key={menu.link} // 각 메뉴는 고유한 키값을 가져야 합니다.
-                    link={menu.link}
+                    key={menu.title} // 메뉴마다 고유한 키를 사용
+                    link={menu.serviceLink}
                     iconSrc={menu.iconSrc}
-                    text1={menu.text1}
-                    text2={menu.text2}
-                    hashtag1={menu.hashtag}
+                    text1={menu.title}
+                    text2={menu.contents}
+                    hashtag1={menu.hashtag} // menu 객체의 hashtag 필드 사용
                 />
             ))}
         </>
     );
 };
- */
