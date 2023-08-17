eee0a2320c9749aa337e70981f28642abf33b4f9
DEGULL <skytmdgus99@gmail.com>
2023-08-17 09:02:01 +0900

    hashtag API

--- components/CategoryComponents/Public/Category/Comprehensive.js
+++ components/CategoryComponents/Public/Category/Comprehensive.js
@@ -1,8 +1,23 @@
-/* import React from 'react';
-import Link from 'next/link'
+import React, { useState, useEffect } from 'react';
 import MenuBox from '../../../../components/AppComponents/MenuBox';
 
 export default function Content() {
+    const [menuData, setMenuData] = useState([]); // 메뉴 데이터를 저장할 상태
+
+    useEffect(() => {
+        // API에서 메뉴 데이터를 가져오는 함수
+        const fetchMenuData = async () => {
+            try {
+                const response = await axios.get('https://catchkorea-a5799a624288.herokuapp.com/post/{category_id}'); // API 엔드포인트
+                setMenuData(response.data); // API에서 받아온 데이터를 상태에 저장
+            } catch (error) {
+                console.error('Error fetching menu data:', error);
+            }
+        };
+
+        fetchMenuData(); // 메뉴 데이터를 가져오는 함수 호출
+    }, []);
+
     return (
         <>
         <MenuBox
@@ -10,41 +25,41 @@ export default function Content() {
             iconSrc='\AppIcon\Goverment24.png'
             text1='Goverment 24'
             text2='The most popular Korean Bus application'
-            hashtag1='#public'
+            hashtag1={menu.hashtag}
         />
         <MenuBox
             link="/InfoComponents/Public/Comprehensive/MobileID"
             iconSrc='\AppIcon\MobileID.png'
             text1='Mobile ID'
             text2='The most popular Korean Bus application'
-            hashtag1='#public'
+            hashtag1={menu.hashtag}
         />    
         <MenuBox 
             link = "/InfoComponents/Public/Comprehensive/Pass" 
             iconSrc = '\AppIcon\Pass.png' 
             text1 = 'Pass' 
             text2 = 'The most popular Korean subway application' 
-            hashtag1 = '#public' 
+            hashtag1={menu.hashtag}
         /> 
         <MenuBox
             link="/InfoComponents/Public/Comprehensive/Hometax"
             iconSrc='\AppIcon\Hometax.png'
             text1='Hometax'
             text2='The most popular Korean Bus application'
-            hashtag1='#public'
+            hashtag1={menu.hashtag}
         />
         <MenuBox
             link="/InfoComponents/Public/Comprehensive/Jiro"
             iconSrc='\AppIcon\Jiro.png'
             text1='Mobile Jiro'
             text2='The most popular Korean Bus application'
-            hashtag1='#public'
+            hashtag1={menu.hashtag}
         />
     </>
     );
 };
- */
-
+ 
+/* 
 import React, { useState, useEffect } from 'react';
 import MenuBox from '../../../../components/AppComponents/MenuBox';
 import axios from 'axios';
@@ -81,3 +96,4 @@ export default function Content() {
         </>
     );
 };
+ */
--- components/MenuComponents/MenuBar.js
+++ components/MenuComponents/MenuBar.js
@@ -8,8 +8,8 @@ export default function MenuBar({ menuVisible, onClick }) {
             
             <S.CloseButton onClick={onClick} src='/img/free-icon-x-657059.png'></S.CloseButton>
 
-            <a href="/MainComponents/Main/Main"><S.MenuIcon src='/img/Home.png' /></a>
-            <S.MenuText src='/img/CatchKorea.png'/>
+{/*             <a href="/MainComponents/Main/Main"><S.MenuIcon src='/img/Home.png' /></a>
+ */}            <S.MenuText src='/img/CatchKorea.png'/>
          </S.Container>
 
 
