4387edd7edde9eecc0335ce6eb28bc4ee8d01699
DEGULL <skytmdgus99@gmail.com>
2023-08-17 13:07:42 +0900

    tipbox&FAQ 수정

--- pages/CategoryComponents/FAQ/FAQ.js
+++ pages/CategoryComponents/FAQ/FAQ.js
@@ -2,13 +2,13 @@ import React from 'react';
 import * as S from './FAQ.styled'
 
 import Header from '../../../components/MainComponents/Header';
-import Body from '../../../components/CategoryComponents/App/Body';
 
 export default function FAQ(){
    return (
       <S.Wrapper>
          <S.Container>
             <Header />
+            <h1>FAQ</h1>
          </S.Container>
       </S.Wrapper>
    );
