4ad6bc18e21700b31db5ec2ce63d90383ec52710
DEGULL <skytmdgus99@gmail.com>
2023-08-17 10:56:37 +0900

    tipbox toggle 추가

--- components/TipComponents/TipBox.js
+++ components/TipComponents/TipBox.js
@@ -1,17 +1,182 @@
-import React from 'react';
+/* import React, { useState } from 'react';
 import * as S from "./TipBox.styled";
-import Link from 'next/link'
+import { keyframes } from "@emotion/react";
 
-export default function TipBox() {
+export default function TipBox(){
+    const rotateAnimation = keyframes`
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(90deg);
+        }
+    `;
+
+    const [isTipVisible, setIsTipVisible] = useState(false);
+    const [isImageRotated, setIsImageRotated] = useState(false);
+
+    const handleToggleClick = () => {
+        setIsTipVisible(!isTipVisible);
+        setIsImageRotated(!isImageRotated);
+    };
+
     return (
         <S.Container>
-            <Link href="/CategoryComponents/Tip">
-                <S.TipBox>
-                    <S.TipIcon src='\img\Tip.svg'/>
-                    <S.TipText>Transportation Tips</S.TipText>
-                    <S.MoveImg src='\img\Move.svg'/>
-                </S.TipBox>
-            </Link>
+            <S.TipBox expanded={isTipVisible}>
+                <S.TipIcon src='/img/Tip.svg'/>
+                <div>
+                    <S.TipTitle>Transportation Tips</S.TipTitle>
+                    {isTipVisible && (
+                        <S.TipText>Transfer refers to transferring from the transportation you were riding...</S.TipText>
+                    )}
+                </div>
+                <S.MoveImg
+                    onClick={handleToggleClick}
+                    src='/img/toggle1.png'
+                    className={isImageRotated ? 'rotate-animation' : ''}
+                />
+            </S.TipBox>
         </S.Container>
     );
 };
+ */
+
+/* import React, { useState } from 'react';
+import * as S from "./TipBox.styled";
+import { keyframes } from "@emotion/react";
+
+export default function TipBox() {
+    const rotateAnimation = keyframes`
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(90deg);
+        }
+    `;
+
+    const [isTipVisible, setIsTipVisible] = useState(false);
+
+    const handleToggleClick = () => {
+        setIsTipVisible(!isTipVisible);
+    };
+
+    return (
+        <S.Container>
+            <S.TipBox expanded={isTipVisible}>
+                <S.TipIcon src='/img/Tip.svg'/>
+                <div>
+                    <S.TipTitle>Transportation Tips</S.TipTitle>
+                    {isTipVisible && (
+                        <S.TipText>Transfer refers to transferring from the transportation you were riding...</S.TipText>
+                    )}
+                </div>
+                <S.MoveImg
+                    onClick={handleToggleClick}
+                    src='/img/toggle1.png'
+                    className={isTipVisible ? 'rotate-animation' : ''}
+                />
+            </S.TipBox>
+        </S.Container>
+    );
+};
+
+
+
+ */
+/* 
+import React, { useState } from 'react';
+import * as S from "./TipBox.styled";
+
+export default function TipBox() {
+    const [isTipVisible, setIsTipVisible] = useState(false);
+    const [isImageRotated, setIsImageRotated] = useState(false);
+
+    const handleToggleClick = () => {
+        setIsTipVisible(!isTipVisible);
+        setIsImageRotated(!isImageRotated);
+    };
+
+    return (
+        <S.Container>
+            <S.TipBox expanded={isTipVisible}>
+                <S.TipIcon src='/img/Tip.svg'/>
+                <S.TipTitle onClick={handleToggleClick}>Transportation Tips</S.TipTitle>
+                <S.MoveImg
+                    onClick={handleToggleClick}
+                    src='/img/toggle1.png'
+                    className={isImageRotated ? 'rotate-animation' : ''}
+                />
+                <S.TipText expanded={isTipVisible}>
+                    Transfer refers to transferring from the transportation you were riding...
+                </S.TipText>
+            </S.TipBox>
+        </S.Container>
+    );
+} */
+
+/* import React, { useState } from 'react';
+import * as S from "./TipBox.styled";
+
+export default function TipBox() {
+    const [isTipVisible, setIsTipVisible] = useState(false);
+    const [isImageRotated, setIsImageRotated] = useState(false);
+
+    const handleToggleClick = () => {
+        setIsTipVisible(!isTipVisible);
+        setIsImageRotated(!isImageRotated);
+    };
+
+    return (
+        <S.Container>
+            <S.TipBox expanded={isTipVisible}>
+                <S.TipIcon src='/img/Tip.svg'/>
+                <S.TipTitle onClick={handleToggleClick}>Transportation Tips</S.TipTitle>
+                <S.MoveImg
+                    onClick={handleToggleClick}
+                    src='/img/toggle1.png'
+                    className={isImageRotated ? 'rotate-animation' : ''}
+                />
+                {isTipVisible && (
+                    <S.TipText>
+                        Transfer refers to transferring from the transportation you were riding...
+                    </S.TipText>
+                )}
+            </S.TipBox>
+        </S.Container>
+    );
+}
+ */
+
+import React, { useState } from 'react';
+import * as S from "./TipBox.styled";
+
+export default function TipBox() {
+    const [isTipVisible, setIsTipVisible] = useState(false);
+    const [isImageRotated, setIsImageRotated] = useState(false);
+
+    const handleToggleClick = () => {
+        setIsTipVisible(!isTipVisible);
+        setIsImageRotated(!isImageRotated);
+    };
+
+    return (
+        <S.Container>
+            <S.TipBox expanded={isTipVisible}>
+                <div onClick={handleToggleClick}>
+                    <S.TipIcon src='/img/Tip.svg'/>
+                    <S.TipTitle>Transportation Tips</S.TipTitle>
+                    <S.MoveImg
+                        src='/img/toggle1.png'
+                        className={isImageRotated ? 'rotate-animation' : ''}
+                    />
+                </div>
+                {isTipVisible && (
+                    <S.TipText>Transfer refers to transferring from the transportation you were riding to the same or different transportation.
+                        People usually transfer when transportation doesn't go to their destination at once.
+                        There is no additional charge for each subway and bus transfer, and the transfer between the subway and bus is also free.</S.TipText>
+                )}
+            </S.TipBox>
+        </S.Container>
+    );
+}
--- components/TipComponents/TipBox.styled.js
+++ components/TipComponents/TipBox.styled.js
@@ -1,11 +1,21 @@
 import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
+
+const rotateAnimation = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(90deg);
+    }
+`;
 
 export const Container = styled.div`
 `;
 
 export const TipBox = styled.div`
   width: 271px;
-  height: 37px;
+  min-height: 37px; /* Add a minimum height */
   left: 41px;
   top: 242px;
   background: #FFFFFF;
@@ -15,6 +25,8 @@ export const TipBox = styled.div`
   align-items: center;
   cursor: pointer;
   display: flex;
+  flex-direction: ${props => props.expanded ? 'column' : 'row'}; 
+  transition: min-height 0.3s ease-in-out; 
 `;
 
 export const TipIcon = styled.img`
@@ -22,23 +34,40 @@ export const TipIcon = styled.img`
   width: 21px;
   height: 21px;
   text-align: center;
-  margin: auto 10px;
+  margin: 4px 10px 7px 10px;
 `;
 
-export const TipText = styled.div`
+export const TipTitle = styled.div`
    color: #535353;
    font-size: 12px;
    font-weight: 500;
    text-align: left;
-   margin: auto 40px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   margin: 10px 40px 5px; 
 `;
 
+export const TipText = styled.div`
+color: #535353;
+font-family: Inter;
+font-size: 10px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+text-align: center;
+
+`;
 
 export const MoveImg = styled.img`
-   width: 8px;
-   height: 15px;
-   right: 55px;
-   cursor: pointer;
-   position: absolute;
-   text-align: center;
+    width: 9px;
+    height: 9px;
+    right: 55px;
+    cursor: pointer;
+    position: absolute;
+    margin: -18px 10px;
+
+    &.rotate-animation {
+        animation: ${rotateAnimation} 0.3s linear forwards;
+    }
 `;
